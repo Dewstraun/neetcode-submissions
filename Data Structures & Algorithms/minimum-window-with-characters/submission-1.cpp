ee4704a3a16ce9a90499ddc30275ad9c30ffc2ba
dewstraun <ishivamkhandelwal@gmail.com>
2026-08-12 20:19:47 +0530

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left=0, right=0, maxlen=0;
+        int hasharr[2]={0};
+        while(right<nums.size()){
+            hasharr[nums[right]]++;
+
+            while(hasharr[0]>k){
+                hasharr[nums[left]]--;
+                left++;
+            }
+            
+            maxlen=max(maxlen,right-left+1);
+            right++;
+        }
+        return maxlen;
+    }
+};
